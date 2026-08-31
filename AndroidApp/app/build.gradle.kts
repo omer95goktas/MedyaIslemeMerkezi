e53243c73d6c80b09f7767ef3010de81b5b5788d
Omer Goktas <omer95goktas@users.noreply.github.com>
2026-08-31 14:52:27 +0300

    Fix video conversion formats bug and bump version to 1.0.21

--- AndroidApp/app/build.gradle.kts
+++ AndroidApp/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "net.omergoktas.medyaisleme"
         minSdk = 24
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.0.19"
+        versionCode = 23
+        versionName = "1.0.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
